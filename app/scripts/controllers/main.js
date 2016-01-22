f91f096b0e72206e3c3b85aa5ab583fd4946735e
Matti Peltoniemi <mattpe@metropolia.fi>
2016-01-22 10:28:10 +0200

    step 2 ready

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -38,8 +38,57 @@ angular.module('imageEditorApp')
 
     $scope.resetImage = function () {
       // when image data is loaded, (after onload)
+      // set size of canvas to match image size
+      $scope.canvas.height = $scope.image.height;
+      $scope.canvas.width = $scope.image.width;
+
       // put the data into canvas element
       $scope.ctx.drawImage($scope.image, 0, 0, $scope.canvas.width, $scope.canvas.height);
+      
+      // read pixel data
+      $scope.imageData = $scope.ctx.getImageData(0, 0, $scope.canvas.width, $scope.canvas.height);
+      $scope.pixels = $scope.imageData.data;
+      $scope.numPixels = $scope.imageData.width * $scope.imageData.height;
     };
+  
+  
+    // Generic method for resetting image, applying filters and updating canvas
+    $scope.applyFilters = function() {
+      $scope.resetImage();
+
+      setBrightness();
+      setContrast();
+
+      $scope.ctx.clearRect(0, 0, $scope.canvas.width, $scope.canvas.height);
+      $scope.ctx.putImageData($scope.imageData, 0, 0);
+    };
+
+    // FILTERS
+    
+    var setBrightness = function () {
+      // type of input field value is string and must be parsed to int to make
+      // numeric calculations instead of string concatenation
+      var brightnessInt = parseInt($scope.brightness);
+      // iterate through pixel array and modify values of each pixel one by one 
+      for (var i = 0; i < $scope.numPixels; i++) {
+        $scope.pixels[i * 4] += brightnessInt; // Red
+        $scope.pixels[i * 4 + 1] += brightnessInt; // Green 
+        $scope.pixels[i * 4 + 2] += brightnessInt; // Blue
+      }
+    };  
+
+    var setContrast = function () {
+      // type of input field value is string and must be parsed to float to make
+      // numeric calculations instead of string concatenation
+      var contrastFloat = parseFloat($scope.contrast);
+      // iterate through pixel array and modify rgb values of each pixel one by one 
+      for (var i = 0; i < $scope.numPixels; i++) {
+        $scope.pixels[i * 4] = ($scope.pixels[i * 4] - 128) * contrastFloat + 128; // Red
+        $scope.pixels[i * 4 + 1] = ($scope.pixels[i * 4 + 1] - 128) * contrastFloat + 128; // Green
+        $scope.pixels[i * 4 + 2] = ($scope.pixels[i * 4 + 2] - 128) * contrastFloat + 128; // Blue
+        
+      }
+    };  
+
 
 });
